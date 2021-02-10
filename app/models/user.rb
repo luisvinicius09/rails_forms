45f3e1adb2bdef6da6d4e4e3143ed36dd7d3a156
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-02-10 18:28:11 -0400

    Updated length range

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: [minimum: 3, maximum: 15]
-  validates :email, presence: true, length: [minimum: 4, maximum: 20]
-  validates :password, presence: true, length: [minimum: 6, maximum: 25]
+  validates :username, presence: true, length: { in: 3..15 }
+  validates :email, presence: true, length: { in: 4..20 }
+  validates :password, presence: true, length: { in: 6..25 }
 end
